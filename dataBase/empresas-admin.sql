1ff0a221ff0dd29b12cf604fdb229c819c874ee4
Theo <theostracke11@gmail.com>
2025-08-11 10:49:36 -0300

    Swap order of empresa insert statements
    Reordered the INSERT statements in empresas-admin.sql so that 'Rede Vellum Admin' appears after 'Estrada Fácil Admin'. No data was changed, only the order of insertion.

--- dataBase/empresas-admin.sql
+++ dataBase/empresas-admin.sql
@@ -1,5 +1,5 @@
 
 
 INSERT INTO empresa (cnpj, razao_social, email, senha, tipo) VALUES 
-('43.403.910/0001-28', 'Estrada Fácil Admin ', 'suporte@estradafacil.com.br', '3str@adaF2c1il#', 'ESTRADA_FACIL'),
-('20.692.051/0001-39', 'Rede Vellum Admin', 'docuflowrepositoryrv@gmail.com', '#r3d3v3llum@1029384756', 'REDE_VELLUM');
+('20.692.051/0001-39', 'Estrada Fácil Admin ', 'suporte@estradafacil.com.br', '3str@adaF2c1il#', 'ESTRADA_FACIL'),
+('43.403.910/0001-28', 'Rede Vellum Admin', 'docuflowrepositoryrv@gmail.com', '#r3d3v3llum@1029384756', 'REDE_VELLUM');
